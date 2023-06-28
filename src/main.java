dcc01ca51103199c0e2496e045f743fc22f25dbe
Ivan Kashuba <kashubaim@gmail.com>
2023-06-28 20:08:55 +0600

    second commit

--- src/main.java
+++ src/main.java
@@ -1,14 +1,14 @@
 public class main {
     public static void main(String[] args) {
 
-        int m = 1100;     // Сумма пополнения
-        int n = 100;     // Имеющиеся на счету средства
+        int payment = 1100;             // Сумма пополнения
+        int balance = 100;              // Имеющиеся на счету средства
 
-        if (m >= 1000) {
-            System.out.println(n + m + (m / n) + " рублей (-я) на счету");
-            System.out.println((m / n) + " бонусных рублей");
+        if (payment >= 1000) {
+            System.out.println(balance + payment + (payment / balance) + " рублей (-я) на счету");
+            System.out.println((payment / balance) + " бонусных рублей");
         } else {
-            System.out.println(n + m + " рублей (-я) на счету");
+            System.out.println(balance + payment + " рублей (-я) на счету");
         }
     }
 }
